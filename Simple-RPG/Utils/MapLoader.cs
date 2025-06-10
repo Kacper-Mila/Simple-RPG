67ca1b3d90a3fec851e9ac7a3866bd82b5098522
Kacper <kacper.mila@gmail.com>
2025-06-10 22:55:48 +0200

    made the path better for running the project with dotnet run command

--- Simple-RPG/Utils/MapLoader.cs
+++ Simple-RPG/Utils/MapLoader.cs
@@ -4,7 +4,7 @@ public static class MapLoader
 {
     public static string[] LoadMapFromFile(string mapName)
     {
-        var modifiedPath = "../../../Maps/maps/" + mapName;
+        var modifiedPath = "Maps/maps/" + mapName;
         if (File.Exists(modifiedPath)) return File.ReadAllLines(modifiedPath);
         Console.WriteLine("Map file not found.");
         return [];
